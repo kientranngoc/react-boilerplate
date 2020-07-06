d0814c21441152d7f31c2a73573b5fe4da4def88
Tran Kien <kien@gotitapp.co>
2020-07-06 22:16:55 +0700

    feat: Asynchronously Load webpack Bundles through Code-splitting and React Suspense

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
 
+const Warning = React.lazy(() => import('./Warning'))
+
 class App extends React.Component {
   state = {
     count: 0
@@ -12,6 +14,7 @@ class App extends React.Component {
         <h2>Count: {this.state.count}</h2>
         <button onClick={() => this.setState(state => ({count: this.state.count+1}))}>+</button>
         <button onClick={() => this.setState(state => ({count: this.state.count-1}))}>-</button>
+        {this.state.count > 10 ? <React.Suspense fallback={null}><Warning /></React.Suspense> : null}
       </div>
     )
   }
