bf93dfab011a5fa1e1ab1041a9f8c9d31e89a1e7
Tran Kien <kien@gotitapp.co>
2020-07-07 08:47:41 +0700

    fix: Fix increase/decrease button onClick logic

--- src/App.js
+++ src/App.js
@@ -12,12 +12,12 @@ class App extends React.Component {
         <h1>Hello World</h1>
         <h2>Count: {this.state.count}</h2>
         <button
-          onClick={() => this.setState(() => ({ count: this.state.count + 1 }))}
+          onClick={() => this.setState((state) => ({ count: state.count + 1 }))}
         >
           +
         </button>
         <button
-          onClick={() => this.setState(() => ({ count: this.state.count - 1 }))}
+          onClick={() => this.setState((state) => ({ count: state.count - 1 }))}
         >
           -
         </button>
